76e7a0893e2568d00cbd8a2da1761129264f122c
erick.silva <erick.silva@ifood.com.br>
2022-05-30 20:10:47 -0300

    feat(items): Adding item details get

--- index.ts
+++ index.ts
@@ -12,12 +12,13 @@ const axiosInstance = axios.create({
   baseURL: "https://api.mercadolibre.com/",
 });
 
+const author =  {
+  name: "Erick",
+  lastname: "Silva",
+};
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
 
 app.get('/api/items', async (req: Request, res: Response) => {
@@ -45,13 +46,41 @@ app.get('/api/items', async (req: Request, res: Response) => {
 
   const categories = data.filters[0]?.values[0]?.path_from_root?.map(({ name }: { name:string }) => name) || [];
   const response = {
-    autor: {
-      name: "Erick",
-      lastname: "Silva",
-    },
+    author,
     categories,
     items,
   }
 
   res.json(response);
+});
+
+app.get('/api/items/:id', async (req: Request, res: Response) => {
+  const id = req.params.id
+  const { data: itemResponse } = await axiosInstance.get(`/items/${id}`)
+  const { data: itemCategoryResponse } = await axiosInstance.get(`/categories/${itemResponse.category_id}`)
+  const { data: itemDescriptionResponse } = await axiosInstance.get(`/items/${id}/description`)
+ 
+  const response = {
+    author,
+    categories: itemCategoryResponse.path_from_root.map(({ name }: { name:string }) => name),
+    item: {
+      id: itemResponse.id,
+      title: itemResponse.title,
+      price: {
+        currency: itemResponse.currency_id,
+        amount: itemResponse.price,
+      },
+      picture: itemResponse.pictures[0].secure_url,
+      condition: itemResponse.condition,
+      free_shipping: itemResponse.shipping.free_shipping,
+      sold_quantity: itemResponse.sold_quantity,
+      description: itemDescriptionResponse.plain_text,
+    }
+  }
+
+  res.json(response);
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
